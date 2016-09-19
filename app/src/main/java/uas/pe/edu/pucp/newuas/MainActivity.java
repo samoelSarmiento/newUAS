f000126a65c0363e666a4dedbed287d2163fa080
samoelSarmiento <samoel.sarmiento@pucp.pe>
2016-09-19 00:23:42 -0500

    first commit

--- app/src/main/java/uas/pe/edu/pucp/newuas/MainActivity.java
+++ app/src/main/java/uas/pe/edu/pucp/newuas/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //iniciado cosas
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
