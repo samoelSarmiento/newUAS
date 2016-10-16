a5c3c9afc7685375564d87559b18e476c845f6f8
samoelSarmiento <samoel.sarmiento@pucp.pe>
2016-10-16 18:10:33 -0500

    añadi un splash screen para que aparesca el logo de uas y luego se va al login. se ve boni :'v xd pero algo es algo :v

--- app/src/main/java/uas/pe/edu/pucp/newuas/view/MainActivity.java
+++ app/src/main/java/uas/pe/edu/pucp/newuas/view/MainActivity.java
@@ -32,12 +32,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btModuleAcr:
-
-
-
-
-
-
                 break;
         }
     }
