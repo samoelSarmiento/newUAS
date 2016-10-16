9fa11ed01ee8d5164cf0df56b375cf91113f6f4a
samoelSarmiento <samoel.sarmiento@pucp.pe>
2016-10-16 02:18:21 -0500

    vistas

---
+++ app/src/main/java/uas/pe/edu/pucp/newuas/view/MainActivity.java
@@ -0,0 +1,14 @@
+package uas.pe.edu.pucp.newuas.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import uas.pe.edu.pucp.newuas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
