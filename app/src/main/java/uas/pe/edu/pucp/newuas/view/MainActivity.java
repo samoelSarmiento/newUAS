b39f168b78164d67bb2a0492610be7b9cd778f54
samoelSarmiento <samoel.sarmiento@pucp.pe>
2016-10-16 08:33:27 -0500

    cosas

--- app/src/main/java/uas/pe/edu/pucp/newuas/view/MainActivity.java
+++ app/src/main/java/uas/pe/edu/pucp/newuas/view/MainActivity.java
@@ -2,7 +2,6 @@ package uas.pe.edu.pucp.newuas.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import uas.pe.edu.pucp.newuas.R;
 
 public class MainActivity extends AppCompatActivity {
 
