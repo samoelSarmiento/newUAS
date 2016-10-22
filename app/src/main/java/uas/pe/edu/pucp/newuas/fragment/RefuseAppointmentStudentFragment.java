bdfc1ce3f7db91caed4eefb1241e712e60df1486
wingerlion <a20101616@pucp.pe>
2016-10-21 21:44:38 -0500

    tuto update

---
+++ app/src/main/java/uas/pe/edu/pucp/newuas/fragment/RefuseAppointmentStudentFragment.java
@@ -0,0 +1,20 @@
+package uas.pe.edu.pucp.newuas.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uas.pe.edu.pucp.newuas.R;
+
+
+public class RefuseAppointmentStudentFragment extends Fragment {
+
+    public RefuseAppointmentStudentFragment() {
+        // Required empty public constructor
+    }
+
+}
