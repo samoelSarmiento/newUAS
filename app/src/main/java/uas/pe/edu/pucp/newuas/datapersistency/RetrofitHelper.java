42176b5023dea05123927c3ab16896d169bf471a
samoelSarmiento <samoel.sarmiento@pucp.pe>
2016-11-04 18:14:17 -0500

    falto un /

--- app/src/main/java/uas/pe/edu/pucp/newuas/datapersistency/RetrofitHelper.java
+++ app/src/main/java/uas/pe/edu/pucp/newuas/datapersistency/RetrofitHelper.java
@@ -34,7 +34,7 @@ public class RetrofitHelper {
     public static String serverURL = "http://52.89.227.55/api";
 
     public static Retrofit apiConnector = new Retrofit.Builder()
-            .baseUrl("http://52.89.227.55/api")
+            .baseUrl("http://52.89.227.55/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(rxAdapter)
             .client(new OkHttpClient.Builder().addInterceptor(interceptor).build())
