b124909ef6122acfd562c7df49eef60c4d2916bc
wingerlion <a20101616@pucp.pe>
2016-11-20 04:43:08 -0500

    tutoria

---
+++ app/src/main/java/uas/pe/edu/pucp/newuas/fragment/CoordinatorStudentFragment.java
@@ -0,0 +1,33 @@
+package uas.pe.edu.pucp.newuas.fragment;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uas.pe.edu.pucp.newuas.R;
+import uas.pe.edu.pucp.newuas.configuration.Configuration;
+import uas.pe.edu.pucp.newuas.controller.TutCoordinatorController;
+
+
+public class CoordinatorStudentFragment extends Fragment {
+
+    public CoordinatorStudentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_coordinator_student, container, false);
+        TutCoordinatorController ts = new TutCoordinatorController();
+        ts.getStudents(getActivity(),view, Configuration.LOGIN_USER.getUser().getIdUsuario());
+        return view;
+    }
+
+}
