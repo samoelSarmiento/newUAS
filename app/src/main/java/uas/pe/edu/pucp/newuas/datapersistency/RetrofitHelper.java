66a2a5d99aba60f78346b8aefdf0f4768fb99046
José Eduardo Marroquín Rodríguez <jemarroquin@pucp.edu.pe>
2016-11-21 00:55:14 -0500

    merge

--- app/src/main/java/uas/pe/edu/pucp/newuas/datapersistency/RetrofitHelper.java
+++ app/src/main/java/uas/pe/edu/pucp/newuas/datapersistency/RetrofitHelper.java
@@ -34,7 +34,7 @@ public class RetrofitHelper {
 
 
 
-    public static String serverURL = "http://192.168.1.33/internetUAS/public/api/"; // Configuration.BASE_URL + "/api/";  //  "http://192.168.1.33/internetUAS/public/api/" //"http://52.89.227.55/api/";
+   public static String serverURL = "http://192.168.1.33/internetUAS/public/api/"; // Configuration.BASE_URL + "/api/";  //  "http://192.168.1.33/internetUAS/public/api/" //"http://52.89.227.55/api/";
 
     //public static String serverURL = Configuration.BASE_URL + "/api/"; //http://192.168.1.33/internetUAS/public/api/" //"http://52.89.227.55/api/";
 
